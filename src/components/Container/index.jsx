e1e45dd6a0833350ffd51786ccede717b65c5b09
Vamshi krishna <vamshi.kr777@gmail.com>
2024-06-23 18:20:51 +0530

    bug fix

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
-    <div className="w-screen">
+    <div>
       <div className="container mx-auto xs:max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl rounded-xl">
         {children}
       </div>
